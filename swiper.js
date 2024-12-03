c3eb92cf80f101568db7649c82467f74ea33b997
Ivan Nor <ivan.norik@gmail.com>
2024-12-03 15:13:13 +0300

    feat: add swiper for banner - move scripts from assets to root project - add swiper-controls for inline navigation & pagination buttons - add slide photos - fix color dark to rgb

---
+++ swiper.js
@@ -0,0 +1,27 @@
+// var swiper = new Swiper(".swiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
