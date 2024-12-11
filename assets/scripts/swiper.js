25ff035dce9f72592274d36215abbb75eb107669
Ivan Nor <ivan.norik@gmail.com>
2024-12-11 11:32:55 +0300

    [main] fix: move scrits to asset folder

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -1,17 +1,3 @@
-// var swiper = new Swiper(".swiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 const bannerSwiper = new Swiper('.banner__swiper', {
   loop: true,
   slidesPerView: 1,
